1d92212ad464fa45c800c151a038483ca7e40550
MicksCoder <mickaelgoliveira@gmail.com>
2024-04-23 12:31:09 -0300

    versão 2.0

---
+++ galeria-de-arte/PaginasScript.js
@@ -0,0 +1,90 @@
+document.addEventListener("DOMContentLoaded",function(){
+    var meuLink = document.getElementById("1")
+    meuLink.addEventListener("click",function(event){
+        event.preventDefault()
+        var url = this.getAttribute("href")
+        console.log(url)
+        window.open(url,"_blank")
+    })
+})
+document.addEventListener("DOMContentLoaded",function(){
+    var meuLink = document.getElementById("2")
+    meuLink.addEventListener("click",function(event){
+        event.preventDefault()
+        var url = this.getAttribute("href")
+        console.log(url)
+        window.open(url,"_blank")
+    })
+})
+document.addEventListener("DOMContentLoaded",function(){
+    var meuLink = document.getElementById("3")
+    meuLink.addEventListener("click",function(event){
+        event.preventDefault()
+        var url = this.getAttribute("href")
+        console.log(url)
+        window.open(url,"_blank")
+    })
+})
+document.addEventListener("DOMContentLoaded",function(){
+    var meuLink = document.getElementById("4")
+    meuLink.addEventListener("click",function(event){
+        event.preventDefault()
+        var url = this.getAttribute("href")
+        console.log(url)
+        window.open(url,"_blank")
+    })
+})
+document.addEventListener("DOMContentLoaded",function(){
+    var meuLink = document.getElementById("5")
+    meuLink.addEventListener("click",function(event){
+        event.preventDefault()
+        var url = this.getAttribute("href")
+        console.log(url)
+        window.open(url,"_blank")
+    })
+})
+document.addEventListener("DOMContentLoaded",function(){
+    var meuLink = document.getElementById("6")
+    meuLink.addEventListener("click",function(event){
+        event.preventDefault()
+        var url = this.getAttribute("href")
+        console.log(url)
+        window.open(url,"_blank")
+    })
+})
+document.addEventListener("DOMContentLoaded",function(){
+    var meuLink = document.getElementById("7")
+    meuLink.addEventListener("click",function(event){
+        event.preventDefault()
+        var url = this.getAttribute("href")
+        console.log(url)
+        window.open(url,"_blank")
+    })
+})
+document.addEventListener("DOMContentLoaded",function(){
+    var meuLink = document.getElementById("8")
+    meuLink.addEventListener("click",function(event){
+        event.preventDefault()
+        var url = this.getAttribute("href")
+        console.log(url)
+        window.open(url,"_blank")
+    })
+})
+document.addEventListener("DOMContentLoaded",function(){
+    var meuLink = document.getElementById("9")
+    meuLink.addEventListener("click",function(event){
+        event.preventDefault()
+        var url = this.getAttribute("href")
+        console.log(url)
+        window.open(url,"_blank")
+    })
+})
+document.addEventListener("DOMContentLoaded",function(){
+    var meuLink = document.getElementById("10")
+    meuLink.addEventListener("click",function(event){
+        event.preventDefault()
+        var url = this.getAttribute("href")
+        console.log(url)
+        window.open(url,"_blank")
+    })
+})
---
+++ galeria-de-arte/estilologin.js
@@ -0,0 +1,6 @@
+document.addEventListener(DOMcontentLoaded,function(){
+    var login = document.getElementById("botao")
+    login.addEventListener("click",function(){
+        window.location.href = "http://127.0.0.1:5500/index.html"
+    })
+})
